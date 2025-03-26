ade7e9e92e89b2def030e1d66493e62a2fa89e74
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-03-26 16:02:58 +0100

    Add abstract Building class with sqft property and evacuation warning method

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+class Building {
+    constructor(sqft) {
+      if (new.target === Building) {
+        throw new Error('Building is an abstract class and cannot be instantiated directly');
+      }
+      this._sqft = sqft;
+    }
+  
+    // Getter pour sqft
+    get sqft() {
+      return this._sqft;
+    }
+  
+    // Méthode abstraite à implémenter dans les sous-classes
+    evacuationWarningMessage() {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+  
+  export default Building;

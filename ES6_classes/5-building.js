c9fa5c394c1f24f08c9048338bda235994841ef6
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-03-26 16:14:36 +0100

    Refactor Building class constructor to enforce abstract class rules and ensure subclass implementation of evacuationWarningMessage

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,18 +1,24 @@
 /* eslint-disable */
 class Building {
     constructor(sqft) {
-      this._sqft = sqft;
+        if (new.target === Building) {
+            throw new Error('Building is an abstract class and cannot be instantiated directly');
+        }
+        this._sqft = sqft;
+        if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+            throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
     }
-  
+
     // Getter pour sqft
     get sqft() {
-      return this._sqft;
+        return this._sqft;
     }
-  
+
     // Méthode abstraite à implémenter dans les sous-classes
     evacuationWarningMessage() {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+        throw new Error('Class extending Building must override evacuationWarningMessage');
     }
 }
-  
+
 export default Building;

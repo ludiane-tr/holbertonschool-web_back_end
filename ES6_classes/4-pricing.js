830fd857350f5b918bf81c1fd39abcedca86b925
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-03-26 15:24:36 +0100

    Add Pricing class with getters, setters, and methods for price display and conversion

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,1 +1,50 @@
 /* eslint-disable */
+import Currency from './3-currency.js';
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter pour amount
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter pour amount
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Getter pour currency
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter pour currency
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  // Méthode pour afficher le prix avec la devise
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Méthode statique pour convertir un prix
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversion rate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;

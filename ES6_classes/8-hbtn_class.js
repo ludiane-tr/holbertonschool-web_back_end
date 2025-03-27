f1cf27de6db265b302348fb26bebd507d5e46188
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-03-27 09:13:50 +0100

    Add HolbertonClass with constructor, valueOf, and toString methods

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -1,1 +1,20 @@
 /* eslint-disable */
+class HolbertonClass {
+    constructor(size, location) {
+      this._size = size;
+      this._location = location;
+    }
+  
+    // Cast en Number => retourne la taille (size)
+    valueOf() {
+      return this._size;
+    }
+  
+    // Cast en String => retourne la localisation (location)
+    toString() {
+      return this._location;
+    }
+  }
+  
+  export default HolbertonClass;
+  

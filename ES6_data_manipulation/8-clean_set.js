dab3d908e9ea36d66929773643f727416e33d9ba
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-04-01 09:47:00 +0200

    Add functions to check array values in a set and clean set based on a starting string

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+/*eslint-disable*/
+export default function cleanSet(set, startString) {
+    if (!startString || typeof startString !== 'string') {
+      return '';
+    }
+    return Array.from(set)
+      .filter((values) => values.startsWith(startString) && typeof values === 'string')
+      .map((values) => values.slice(startString.length)).join('-');
+  }
+  

c0cb3527f4dfef20475fa02d59aeaf5707e461d8
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-04-23 13:51:54 +0200

    Add 1-concat.py module with string concatenation function and docstring

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module providing a function that concatenate two string and return their concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that concatenate two strings"""
+    return str1 + str2

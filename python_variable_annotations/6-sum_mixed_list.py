9053a6e219935d8396dd74cc846b12695c400e87
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-04-23 14:54:57 +0200

    Fix docstring and type annotations in sum_mixed_list function

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Module providing function that take mixed type and return a sum of the float
+Module providing a function that take mixed type and return a sum of the float
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     Function that return the sum of the float
     """
-    return float(sum(mxd_lst))
+    return sum(mxd_lst)

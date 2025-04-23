9f852a8258eb30e94b96cb954138790569ee3e26
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-04-23 11:15:53 +0200

    execute the correction

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-"""Module providing a function to add float"""
-
+"""Module providing a function to add two float"""
 
 def add(a: float, b: float) -> float:
- """Function to add two numbers and return their sum"""   
+    """Function to add two numbers and return their sum (float)"""   
     return a + b

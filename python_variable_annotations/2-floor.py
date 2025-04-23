518c289d4486cb7cdd1cb1fde00eaefdc44325b2
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-04-23 14:07:36 +0200

    Add floor function and update main files with annotations and print statements

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module providing a function that take a float and return the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """Function that return the floor of the float"""
+    return int(n)

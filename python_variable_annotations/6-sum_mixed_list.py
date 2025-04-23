5037171b864b3733aadf523411323f9dbda81802
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-04-23 14:54:14 +0200

    execute the correction

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-''' Type annotated sum_mixed_list '''
-
-
+"""
+Module providing function that take mixed type and return a sum of the float
+"""
 from typing import List
 
 
 def sum_mixed_list(mxd_lst: List[int | float]) -> float:
-    '''Documentation mixed sum list'''
+    """
+    Function that return the sum of the float
+    """
     return float(sum(mxd_lst))

7c9f6e8fc29d127ba20f61d3ae6a88913902dee4
ludiane-tr <ludiane.trouillefou@gmail.com>
2025-04-23 11:17:13 +0200

    Fix formatting in 0-add.py by removing extra newline and ensuring consistent docstring indentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module providing a function to add two float"""
 
+
 def add(a: float, b: float) -> float:
-    """Function to add two numbers and return their sum (float)"""   
+    """Function to add two numbers and return their sum (float)"""
     return a + b
